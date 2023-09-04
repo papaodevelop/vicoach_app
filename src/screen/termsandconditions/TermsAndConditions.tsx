29e5ae4a5ad5111806e6a327a56abdc3656ed575
kienvipaz99 <kienvipaz99@gmail.com>
2023-09-04 15:31:58 +0700

    kien

--- src/screen/termsandconditions/TermsAndConditions.tsx
+++ src/screen/termsandconditions/TermsAndConditions.tsx
@@ -2,16 +2,20 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
   useWindowDimensions,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import stylescustom from '../../res/stylescustom';
 import HeaderScreen from '../../component/header/HeaderScreen';
 import {NavigationProp} from '@react-navigation/native';
 import RenderHtml from 'react-native-render-html';
 import {useGetTermPageQuery} from '../../redux/state';
 import Loading from '../../component/loading/Loading';
+import BTNLogin from '../../component/btn/BTNLogin';
+import ModalConfirm from '../../component/modal/ModalConfirm';
+import {Use} from 'react-native-svg';
 
 const TermsAndConditions = ({
   navigation,
@@ -19,7 +23,9 @@ const TermsAndConditions = ({
   navigation: NavigationProp<Record<string, any>>;
 }) => {
   const {width} = useWindowDimensions();
-  const {data, isLoading} = useGetTermPageQuery('');
+  const [show, setShow] = useState(false);
+
+  const {data, isLoading, refetch, isFetching} = useGetTermPageQuery('');
   return (
     <View style={stylescustom.container}>
       <HeaderScreen navigation={navigation} title="Điều khoản điều kiện" />
@@ -31,8 +37,22 @@ const TermsAndConditions = ({
             html: `${data?.content?.vi}`,
           }}
         />
+        <View style={{alignItems: 'center', marginBottom: 50, marginTop: 20}}>
+          <BTNLogin
+            txt="Yêu cầu xoá tài khoản"
+            onPress={async () => {
+              setShow(true);
+            }}
+          />
+        </View>
       </ScrollView>
       {isLoading && <Loading />}
+      <ModalConfirm
+        txt="Yêu cầu xoá tài khoản của bạn đã được gửi đi. Cảm ơn bạn đã tin dùng chúng tôi!"
+        isShow={show}
+        confirm={() => setShow(false)}
+        toggleDate={() => setShow(false)}
+      />
     </View>
   );
 };
