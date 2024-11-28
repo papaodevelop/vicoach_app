2b3acb8ca339c72ed2f3b6066a21bf851dd9ac66
HoangCuong <cuongdevmb@gmail.com>
2024-11-28 10:40:44 +0700

    change name app

--- android/app/src/main/java/com/vi_mater/MainActivity.java
+++ android/app/src/main/java/com/vi_mater/MainActivity.java
@@ -1,4 +1,4 @@
-package com.vi_mater;
+package com.nlp;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Vi_Mater";
+    return "NLP_Training";
   }
 
   /**
--- android/app/src/main/java/com/vi_mater/MainApplication.java
+++ android/app/src/main/java/com/vi_mater/MainApplication.java
@@ -1,4 +1,4 @@
-package com.vi_mater;
+package com.nlp;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
